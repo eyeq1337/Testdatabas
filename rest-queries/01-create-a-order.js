f49227c76b16305cc134be57ef2a72130f41c3db
eyeq1337 <eyeq24@gmail.com>
2019-10-17 12:59:56 +0200

    my trys sucks

---
+++ rest-queries/01-create-a-order.js
@@ -0,0 +1,15 @@
+// Note: Using should test syntax
+module.exports = ({ response, store, repeat, }) => ({
+  path: 'order.rows',
+  method: 'post',
+  body: store.mockUsers,
+  test() {
+    // store the inserted id in mockUsers
+    store.mockUsers.id = store.insertId;
+    // repeat this step/query as long as
+    // there a more mockUsers to insert
+    if(store.mockUsers){
+      repeat();
+    }
+  }
+});
