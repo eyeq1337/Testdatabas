e94686a38d022924fef4823c91c1efc203c5c545
eyeq1337 <eyeq24@gmail.com>
2019-10-21 21:55:23 +0200

    hdf

--- rest-queries/17-get-create-products.js
+++ rest-queries/17-get-create-products.js
@@ -1,20 +1,8 @@
 // Note: Using assert test syntax
-module.exports = ({ assert, response, store }) => ({
-    path: 'orders',
-    path: 'prudocts',
+module.exports = ({ expect, response }) => ({
+    path: 'allProData',
     method: 'get',
     test() {
-      // check that we got the right number of users
-      // (intentionelly spelled length wrong to provoke a fail)
-      assert.equal(response.length, store.mockOrders.length);
-      // check that all users where
-      // inserted correctly with correct data
-      assert.deepEqual(response, store.mockOrders);
-       // check that we got the right number of users
-      // (intentionelly spelled length wrong to provoke a fail)
-      assert.equal(response.length, store.mockProducts.length);
-      // check that all users where
-      // inserted correctly with correct data
-      assert.deepEqual(response, store.mockprodcuts);
-    }
-  });
+     expect(response.length).to.equal(5)
+  }
+});
